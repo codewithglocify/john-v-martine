0a76b2bce2a2c8e31bce07e96fb1780dbcd34708
amit kumar <amitkumar@glocify.com>
2026-02-18 17:33:43 +0530

    update header and footer

---
+++ app/config/metadata.ts
@@ -0,0 +1,17 @@
+import type { Metadata } from 'next';
+
+export const defaultMetadata: Metadata = {
+  robots: { index: false, follow: false },
+};
+
+export const homeMetadata: Metadata = {
+  title: 'John V Martine',
+  description: 'John V Martine',
+  ...defaultMetadata,
+};
+
+export const pricingMetadata: Metadata = {
+  title: 'Pricing - John V Martine',
+  description: 'Pricing',
+  ...defaultMetadata,
+};
